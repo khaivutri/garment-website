96877f98e330d74a828346ee5feebcc59f470480
khaivutri <khai.vutri@hcmut.edu.vn>
2026-02-06 11:37:48 +0700

    fix: Size of Login Reqest's DTO

--- backend/src/main/java/com/example/backend/dto/LoginRequest.java
+++ backend/src/main/java/com/example/backend/dto/LoginRequest.java
@@ -1,9 +1,16 @@
 package com.example.backend.dto;
 
 import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 public class LoginRequest {
+    @NotBlank
+    @Size(max = 50)
     private String username;
+
+    @NotBlank
+    @Size(min = 8, max = 64)
     private String password;
 }
